ac458fe64fd1afd04d374fe188175f9e4791d406
EthanYu6 <1037916932@qq.com>
2020-05-28 09:17:38 +0800

    创建父工程

---
+++ src/main/java/com/leyou/parnet/LeyouApplication.java
@@ -0,0 +1,13 @@
+package com.leyou.parnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leyou/parnet/LeyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyou.parnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
